ef754fd3bfcc6112331af6c22a9aa01808d524ed
Alex Tirim <49715292+andyluak@users.noreply.github.com>
2022-11-05 17:14:08 +0200

    Added idle timer

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,34 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import "styles/globals.css";
+import { getAuthCookie, removeAuthCookie } from "utils/cookie";
+import IdleTimer from "utils/idleTimer";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  const signOutUser = () => {
+    const token = getAuthCookie();
+
+    if (token) {
+      removeAuthCookie();
+      router.push("/sign-in").then(() => router.reload());
+    }
+  };
+  useEffect(() => {
+    const timer = new IdleTimer({
+      timeout: 120,
+      onTimeout: () => {
+        signOutUser();
+      },
+      onExpired: () => {
+        signOutUser();
+      },
+    });
+
+    return () => {
+      timer.cleanUp();
+    };
+  }, []);
   const getLayout = Component.getLayout || ((page) => page);
 
   return getLayout(<Component {...pageProps} />);
