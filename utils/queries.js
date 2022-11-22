2c395582b5f79b0ace138257245ad4698d0f957b
Alex Tirim <alexandru.tirim@yahoo.com>
2022-11-22 15:41:42 +0200

    Create delete mutation

--- utils/queries.js
+++ utils/queries.js
@@ -9,3 +9,15 @@ export async function getProducts(auth_token) {
   const data = await res.json();
   return data;
 }
+
+export async function deleteProduct({ auth_token, id }) {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API}/api/product/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${auth_token}`,
+    },
+  });
+  const data = await res.json();
+  return data;
+}
