5e1149dd7c0718eed0f41afd747932e56cbe7499
Alex Tirim <alexandru.tirim@yahoo.com>
2022-11-23 13:14:51 +0200

    Edit mutation

--- containers/ProductListItem.jsx
+++ containers/ProductListItem.jsx
@@ -1,7 +1,7 @@
 import { Dialog, Transition } from "@headlessui/react";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useRouter } from "next/router";
-import { Fragment, useState } from "react";
+import { Fragment, useRef, useState } from "react";
 
 import Modal from "components/ui/Modal";
 
@@ -24,16 +24,26 @@ function ProductListItem({ product: { name, id }, order }) {
   const [newName, setNewName] = useState("");
   const auth_token = getAuthCookie();
 
+  const nameInputRef = useRef(null);
+
   const deleteMutation = useMutation({
-    mutationFn: (variables) => {
+    mutationFn: async (variables) => {
       const { id, auth_token } = variables;
-      fetch(`${process.env.NEXT_PUBLIC_API}/api/product/${id}`, {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${auth_token}`,
-        },
-      });
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API}/api/product/${id}`,
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${auth_token}`,
+          },
+        }
+      );
+
+      if (!res.ok) {
+        const { errors } = await res.json();
+        throw new Error(errors);
+      }
     },
     onMutate: async (variables) => {
       await queryClient.cancelQueries(["products"]);
@@ -52,7 +62,56 @@ function ProductListItem({ product: { name, id }, order }) {
     },
     onSettled: () => {
       setIsOpen(false);
-      // queryClient.invalidateQueries(["products"]);
+      queryClient.invalidateQueries(["products"]);
+    },
+  });
+
+  const editMutation = useMutation({
+    mutationFn: async (variables) => {
+      const { newName: name, auth_token, id } = variables;
+      if (name.length === 0) {
+        throw new Error();
+      }
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API}/api/product/${id}`,
+        {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${auth_token}`,
+          },
+          body: JSON.stringify({ name }),
+        }
+      );
+
+      if (!res.ok) {
+        const { errors } = await res.json();
+        throw new Error(errors);
+      }
+    },
+    onMutate: async (variables) => {
+      await queryClient.cancelQueries(["products"]);
+
+      const previousProducts = queryClient.getQueryData(["products"]);
+
+      queryClient.setQueryData(["products"], (oldProducts) => {
+        oldProducts.map((product) => {
+          if (product.id == variables.id) {
+            product.name = variables.name;
+          }
+          return product;
+        });
+      });
+
+      return { previousProducts };
+    },
+    onError: (error, variables, context) => {
+      queryClient.setQueryData(["products"], context.previousProducts);
+    },
+    onSettled: () => {
+      setIsEditing(false);
+      setNewName("");
+      queryClient.invalidateQueries(["products"]);
     },
   });
 
@@ -97,6 +156,12 @@ function ProductListItem({ product: { name, id }, order }) {
             className="tracking-body inline w-4/5 border-x-0 border-t-0 text-left text-xl leading-[18px] focus:outline-0  focus:ring-0 md:w-1/2 md:text-left"
             onChange={(e) => setNewName(e.target.value)}
             value={newName}
+            onKeyDown={(e) => {
+              if (e.code === "Enter") {
+                editMutation.mutate({ id, auth_token, newName });
+              }
+            }}
+            ref={nameInputRef}
           />
         ) : (
           <span>{name}</span>
@@ -107,7 +172,7 @@ function ProductListItem({ product: { name, id }, order }) {
           <>
             <button
               className="flex h-16 cursor-pointer flex-col items-center fill-white transition-all hover:border-b-2 hover:border-b-gray-300"
-              onClick={editProduct}
+              onClick={() => editMutation.mutate({ id, auth_token, newName })}
             >
               <Save />
               <p>Save</p>
@@ -124,7 +189,10 @@ function ProductListItem({ product: { name, id }, order }) {
           <>
             <button
               className="flex h-16 cursor-pointer flex-col items-center fill-white transition-all hover:border-b-2 hover:border-b-gray-300"
-              onClick={() => setIsEditing(!isEditing)}
+              onClick={() => {
+                setIsEditing(!isEditing);
+                setTimeout(() => nameInputRef.current.focus(), 100);
+              }}
             >
               <Pencil />
               <p>Edit</p>
